c5653d9a9d7d8717a38173c62ec7a73d82cd5f29
Dennis Iversen <dennis.iversen@gmail.com>
2014-11-08 08:44:07 +0100

    refactor

--- module.php
+++ module.php
@@ -35,9 +35,54 @@ class image {
         die;
     }
     
-    public function ajaxhtmlAction ($url) {
+    public function ajaxhtmlAction($url) {
         $h = new html();
         echo $h->fileHtml5($url);
+    }
+
+    /**
+     * ajax action 
+     * uploads from an ajax request. 
+     */
+    public function ajaxAction() {
+        /**
+         * view file for adding files
+         * @package    image
+         */
+        if (!session::checkAccessFromModuleIni('image_allow_edit')) {
+            moduleloader::setStatus(403);
+            echo lang::translate("Access denied");
+            die();
+        }
+
+        $allow = config::getModuleIni('image_allow_edit');
+        if ($allow == 'user') {
+
+            $table = moduleloader::moduleReferenceToTable($_GET['reference']);
+            if (!user::ownID($table, $_GET['parent_id'], session::getUserId())) {
+                moduleloader::setStatus(403);
+                echo lang::translate("Access denied");
+                die();
+            }
+        }
+
+        $options = array();
+        $options['parent_id'] = $_GET['parent_id'];
+        $options['reference'] = $_GET['reference'];
+
+        image::init($options);
+        image::validateInsert();
+        if (!isset(image::$errors)) {
+            $res = @image::insertFile();
+            if ($res) {
+                echo lang::translate('Image was added');
+            } else {
+                echo reset(image::$errors);
+            }
+        } else {
+            echo reset(image::$errors);
+        }
+        die();
     }
 
     public static function init ($options = null){
