a26bd9a07b6aa8e375524feed465ebf41009d1a6
Dennis Iversen <dennis.iversen@gmail.com>
2015-01-14 22:52:57 +0100

    fix image

--- add.php
+++ add.php
@@ -4,26 +4,25 @@
  * view file for adding files
  * @package    image
  */
+
 if (!session::checkAccessFromModuleIni('image_allow_edit')){
+    
     return;
 }
 
 moduleloader::$referenceOptions = array ('edit_link' => 'true');
 if (!moduleloader::includeRefrenceModule()){   
+    
     moduleloader::setStatus(404);
     return;
 }
 
+
 $options = moduleloader::getReferenceInfo();
+//print_r($options);
 $allow = config::getModuleIni('image_allow_edit');
 
-// if allow is set to user - this module only allow user to edit his own images
-if ($allow == 'user') {
-    if (!user::ownID('image', $options['parent_id'], session::getUserId())) {
-        moduleloader::setStatus(403);
-        return;
-    }   
-}
+
 
 // set headline and title
 $headline = lang::translate('Add image') . MENU_SUB_SEPARATOR_SEC . moduleloader::$referenceLink;
