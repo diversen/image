afda21b764f5c82798129b858021c754ae006315
Dennis Iversen <dennis.iversen@gmail.com>
2015-02-21 10:59:07 +0100

    auto commit

--- add.php
+++ add.php
@@ -1,39 +1,3 @@
 <?php
 
-/**
- * view file for adding files
- * @package    image
- */
 
-if (!session::checkAccessFromModuleIni('image_allow_edit')){
-    
-    return;
-}
-
-moduleloader::$referenceOptions = array ('edit_link' => 'true');
-if (!moduleloader::includeRefrenceModule()){   
-    
-    moduleloader::setStatus(404);
-    return;
-}
-
-
-$options = moduleloader::getReferenceInfo();
-//print_r($options);
-$allow = config::getModuleIni('image_allow_edit');
-
-
-
-// set headline and title
-$headline = lang::translate('Add image') . MENU_SUB_SEPARATOR_SEC . moduleloader::$referenceLink;
-html::headline($headline);
-template::setTitle(lang::translate('Add image'));
-
-// set parent modules menu
-layout::setMenuFromClassPath($options['reference']);
-
-// display image module content
-image::init($options);
-image::viewFileFormInsert($options);
-$rows = image::getAllFilesInfo($options);
-echo image::displayFiles($rows, $options);
--- module.php
+++ module.php
@@ -35,6 +35,43 @@ class image {
         die;
     }
     
+    /**
+     * add action
+     * @return mixed
+     */
+    public function addAction() {
+        if (!session::checkAccessFromModuleIni('image_allow_edit')) {
+            return;
+        }
+
+        moduleloader::$referenceOptions = array('edit_link' => 'true');
+        if (!moduleloader::includeRefrenceModule()) {
+            moduleloader::setStatus(404);
+            return;
+        }
+
+        $options = moduleloader::getReferenceInfo();
+        $allow = config::getModuleIni('image_allow_edit');
+
+        // set headline and title
+        $headline = lang::translate('Add image') . MENU_SUB_SEPARATOR_SEC . moduleloader::$referenceLink;
+        html::headline($headline);
+        template::setTitle(lang::translate('Add image'));
+
+        // set parent modules menu
+        layout::setMenuFromClassPath($options['reference']);
+
+        // display image module content
+        image::init($options);
+        image::viewFileFormInsert($options);
+        $rows = image::getAllFilesInfo($options);
+        echo image::displayFiles($rows, $options);
+    }
+
+    /**
+     * ajaxhtml action (test)
+     * @param type $url
+     */
     public function ajaxhtmlAction($url) {
         $h = new html();
         echo $h->fileHtml5($url);
