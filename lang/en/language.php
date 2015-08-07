4446f031d6b9dc703ad13d81ec95b8f5b816e826
Dennis Iversen <dennis.iversen@gmail.com>
2015-08-07 18:36:48 +0200

    refactor lang

---
+++ lang/en/language.php
@@ -0,0 +1,30 @@
+<?php
+
+$LANG = array();
+
+// Translation of file modules/image/module.php
+
+$LANG['Add image'] = 'Add image';
+$LANG['Delete image'] = 'Delete image';
+$LANG['Delete all images'] = 'Delete all images';
+$LANG['Edit image'] = 'Edit image';
+$LANG['Image was added'] = 'Image was added';
+$LANG['Images'] = 'Images';
+$LANG['Image deleted'] = 'Image deleted';
+$LANG['Delete'] = 'Delete';
+$LANG['Abstract'] = 'Abstract';
+$LANG['Update'] = 'Update';
+$LANG['Add'] = 'Add';
+$LANG['Image width in pixels, e.g. 100'] = 'Image width in pixels, e.g. 100';
+$LANG['No file was specified'] = 'No file was specified';
+$LANG['Edit images'] = 'Edit images';
+$LANG['Download'] = 'Download';
+$LANG['Edit'] = 'Edit';
+$LANG['Image was deleted'] = 'Image was deleted';
+$LANG['All images has been deleted'] = 'All images has been deleted';
+$LANG['Image was updated'] = 'Image was updated';
+$LANG['Access denied'] = 'Access denied';
+$LANG['Access denied'] = 'Access denied';
+// Translation of file modules/image/install.inc
+
+$LANG['Images'] = 'Images';
