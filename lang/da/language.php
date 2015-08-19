2018c01ca860be398fdc68a5a25da8bc2b8d5bfc
Dennis Iversen <dennis.iversen@gmail.com>
2015-08-19 17:11:47 +0200

    da

--- lang/da/language.php
+++ lang/da/language.php
@@ -3,22 +3,20 @@
 $LANG = array();
 
 $LANG['Add image'] = 'Tilføj billede';
-$LANG['Delete image'] = 'Slet billede';
-$LANG['Delete all images'] = 'Slet alle billeder';
 $LANG['Edit image'] = 'Rediger billede';
+$LANG['Delete image'] = 'Slet billede';
+$LANG['Go back'] = 'Gå tilbage';
 $LANG['Image was added'] = 'Billede blev tilsat';
 $LANG['Images'] = 'Images';
 $LANG['Image deleted'] = 'Billede slettet';
 $LANG['Delete'] = 'Slet';
-$LANG['Abstract'] = 'Abstrakt';
 $LANG['Update'] = 'Opdatering';
 $LANG['Add'] = 'Tilføje';
 $LANG['Image width in pixels, e.g. 100'] = 'Billede bredde i pixels, fx 100';
+$LANG['Abstract'] = 'Abstrakt';
 $LANG['No file was specified'] = 'Ingen fil blev angivet';
-$LANG['Edit images'] = 'Redigering af billeder';
 $LANG['Download'] = 'Hent';
 $LANG['Edit'] = 'Edit';
 $LANG['Image was deleted'] = 'Billede blev slettet';
-$LANG['All images has been deleted'] = 'Alle billeder er blevet slettet';
 $LANG['Image was updated'] = 'Billede blev opdateret';
 $LANG['Access denied'] = 'Adgang nægtet';
